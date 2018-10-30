efdc1b6cc0c59b052c39584951b0bed9cd5fa15a
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-30 23:17:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,66 @@
 # your code goes here
+def begins_with_r(array)
+  array.each do |x|
+    if x[0].upcase!="R"
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(array)
+  array.select {|x| x.include? "a"}
+end
+
+def first_wa(array)
+  array.detect{|x| x[0,2]==("wa")}
+end
+
+def remove_non_strings(array)
+  array.delete_if{|x| x.is_a?(String)==false}
+end
+
+def count_elements(hash)
+  output=[]
+  hash.uniq.each{|x| output<<{:count=> hash.count(x), :name=>x[:name]}}
+  output
+end
+
+def merge_data(arr1, arr2)
+  output=[]
+  arr2.each do |namekey|
+    namekey.each do |data, value|
+      arr1.each do |x|
+        if data==x[:first_name]
+          output<<value.merge({:first_name=>data})
+        end
+      end
+    end
+  end
+  output
+end
+
+def find_cool(hashes)
+  output=[]
+  hashes.each do |entry|
+    if entry[:temperature]=="cool"
+      output<<entry
+    end
+  end
+  output
+end
+
+def organize_schools(schools)
+  output={}
+  locations=[]
+  schools.each do |school, data|
+    locations<<data[:location]
+  end
+  locations.uniq.each do |x|
+    output[x]=[]
+  end
+  schools.each do |school, data|
+    output[data[:location]]<<school
+  end
+  output
+end
